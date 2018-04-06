579c839076d3f37b668b71b51226e7aeaee20734
admin <admin>
2018-04-06 20:56:52 +0000

    'first'

---
+++ src/main/java/com/compte/bank/GestionCompteApplication.java
@@ -0,0 +1,12 @@
+package com.compte.bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionCompteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionCompteApplication.class, args);
+	}
+}
